15ba9a2e1219c57fe9879a2c393584f480bcc8e2
ivash1228 <ivash1228@gmail.com>
2024-11-02 16:28:45 -0400

    add one-to-many

--- src/main/java/glushkova/kristina/gym_tracker/entities/ClientEntity.java
+++ src/main/java/glushkova/kristina/gym_tracker/entities/ClientEntity.java
@@ -3,6 +3,8 @@ package glushkova.kristina.gym_tracker.entities;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -18,4 +20,7 @@ public class ClientEntity {
     private String lastName;
     private String email;
     private String phoneNumber;
+
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<WorkoutEntity> workouts = new ArrayList<>();
 }
--- src/main/resources/db/migration/V2__CREATE_WORKOUT_TABLE.sql
+++ src/main/resources/db/migration/V2__CREATE_WORKOUT_TABLE.sql
@@ -2,5 +2,10 @@ CREATE TABLE workout (
     id UUID PRIMARY KEY,
     client_id UUID,
     workout_date DATE,
-    workout_name VARCHAR(30) NOT NULL
+    workout_name VARCHAR(30) NOT NULL,
+
+    CONSTRAINT fk_client
+        FOREIGN KEY (client_id)
+        REFERENCES client(id)
+        ON DELETE CASCADE
 );
