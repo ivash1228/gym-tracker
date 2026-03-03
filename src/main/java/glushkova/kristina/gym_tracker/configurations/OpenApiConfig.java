cdb93c0707af02d746a40301ec04bd403651708e
ivash1228 <ivash1228@gmail.com>
2026-03-03 15:55:14 -0500

    Add actuator endpoint and swagger config

---
+++ src/main/java/glushkova/kristina/gym_tracker/configurations/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package glushkova.kristina.gym_tracker.configurations;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        var securitySchemeName = "bearerAuth";
+        var securityRequirement = new SecurityRequirement().addList(securitySchemeName);
+
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Paste your JWT token (e.g. from Google OAuth login)")))
+                .addSecurityItem(securityRequirement);
+    }
+}
